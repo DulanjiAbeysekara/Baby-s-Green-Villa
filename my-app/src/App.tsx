a11a952aa151da0a20e7999a55e4a7ce38a4247a
DulanjiAbeysekara <dulanjiabeysekara09@gmail.com>
2024-11-07 16:00:27 +0530

    first commit

---
+++ my-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app bg-red-900">
+     App
+    </div>
+  );
+}
+
+export default App;
