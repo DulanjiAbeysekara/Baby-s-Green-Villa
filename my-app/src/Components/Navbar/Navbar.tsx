67318bf69cf9d698bf7038b8d10005e245bb81b5
DulanjiAbeysekara <dulanjiabeysekara09@gmail.com>
2024-11-08 14:55:53 +0530

    creating navbar

---
+++ my-app/src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useState } from "react";
+import {BarsIcon3} from "@heroicons/react";
+
+type Props={};
+
+function Navbar() {
+
+const Navbar=(props:Props)=>{
+
+  return (
+    <div>
+      Navbar
+    </div>
+  )
+}
+
+}
+export default Navbar
